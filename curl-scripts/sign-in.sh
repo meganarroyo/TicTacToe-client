6204a912607397d591771635a0d6858eb8347a41
meganarroyo <megan.ariel@live.com>
2020-07-21 19:34:24 -0500

    Beach Theme Added Boxes and created some of theme

--- curl-scripts/sign-in.sh
+++ curl-scripts/sign-in.sh
@@ -1,6 +1,6 @@
 curl "https://tic-tac-toe-api-development.herokuapp.com/sign-in" \
 --include \
---request POST \
+--request PUSH \
 --header "Content-type: application/json" \
 --data '{
   "credentials": {
